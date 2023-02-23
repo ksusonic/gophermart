80164361b06f46bef1b38e2c43d5e4b4bcf15553
Daniil <ksusonic@yandex.ru>
2023-02-24 01:22:56 +0300

    Init - config + logger

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,46 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/ksusonic/gophermart/internal/config"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func main() {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger := initLogger(cfg.Debug)
+	defer logger.Sync()
+
+	if cfg.Debug {
+		logger.Debugf("loaded cfg: %s", cfg)
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
+
+func initLogger(debug bool) *zap.SugaredLogger {
+	if debug {
+		logger, _ := zap.NewDevelopment()
+		return logger.Sugar()
+	} else {
+		logger, _ := zap.NewProduction()
+		return logger.Sugar()
+	}
+}
